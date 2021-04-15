960c8564887916f2f299cefabe6a1c638cfa926b
Alexis Borges <tuaalexis@gmail.com>
2021-04-15 11:17:11 -0600

    Creacion de pages components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,15 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
+import { FeedComponent } from './components/pages/feed/feed.component';
+import { ProfileComponent } from './components/pages/profile/profile.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavbarComponent
+    NavbarComponent,
+    FeedComponent,
+    ProfileComponent
   ],
   imports: [
     BrowserModule,
